4cc62cc74e392d8309782da0eb12bdc9ab4ecbf2
SatyamReynaMain <mygamingsetup10008@gmail.com>
2025-08-04 14:38:25 +0530

    Update DDL_BRONZE_TABLE.sql

--- Scripts/bronze/DDL_BRONZE_TABLE.sql
+++ Scripts/bronze/DDL_BRONZE_TABLE.sql
@@ -13,7 +13,7 @@ BEGIN
         -- CRM_cust_info
         SET @start_time = GETDATE();
         IF OBJECT_ID('BRONZE.CRM_cust_info', 'U') IS NOT NULL
-            RAISERROR('❌ BRONZE.CRM_cust_info already exists. Creation blocked.', 16, 1);
+            RAISERROR('BRONZE.CRM_cust_info already exists. Creation blocked.', 16, 1);
         ELSE
             CREATE TABLE BRONZE.CRM_cust_info (
                 cst_id INT,
@@ -25,12 +25,12 @@ BEGIN
                 cst_create_date DATE
             );
         SET @end_time = GETDATE();
-        PRINT '✅ BRONZE.CRM_cust_info created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT ' BRONZE.CRM_cust_info created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
         -- CRM_prd_info
         SET @start_time = GETDATE();
         IF OBJECT_ID('BRONZE.CRM_prd_info', 'U') IS NOT NULL
-            RAISERROR('❌ BRONZE.CRM_prd_info already exists. Creation blocked.', 16, 1);
+            RAISERROR('BRONZE.CRM_prd_info already exists. Creation blocked.', 16, 1);
         ELSE
             CREATE TABLE BRONZE.CRM_prd_info (
                 prd_id INT,
@@ -42,12 +42,12 @@ BEGIN
                 prd_end_dt DATETIME
             );
         SET @end_time = GETDATE();
-        PRINT '✅ BRONZE.CRM_prd_info created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'BRONZE.CRM_prd_info created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
         -- CRM_sales_details
         SET @start_time = GETDATE();
         IF OBJECT_ID('BRONZE.CRM_sales_details', 'U') IS NOT NULL
-            RAISERROR('❌ BRONZE.CRM_sales_details already exists. Creation blocked.', 16, 1);
+            RAISERROR('BRONZE.CRM_sales_details already exists. Creation blocked.', 16, 1);
         ELSE
             CREATE TABLE BRONZE.CRM_sales_details (
                 sls_ord_num NVARCHAR(50),
@@ -61,12 +61,12 @@ BEGIN
                 sls_priceINT INT
             );
         SET @end_time = GETDATE();
-        PRINT '✅ BRONZE.CRM_sales_details created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'BRONZE.CRM_sales_details created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
         -- erp_cust_az12
         SET @start_time = GETDATE();
         IF OBJECT_ID('BRONZE.erp_cust_az12', 'U') IS NOT NULL
-            RAISERROR('❌ BRONZE.erp_cust_az12 already exists. Creation blocked.', 16, 1);
+            RAISERROR('BRONZE.erp_cust_az12 already exists. Creation blocked.', 16, 1);
         ELSE
             CREATE TABLE BRONZE.erp_cust_az12 (
                 CID NVARCHAR(50),
@@ -74,24 +74,24 @@ BEGIN
                 GEN VARCHAR(50)
             );
         SET @end_time = GETDATE();
-        PRINT '✅ BRONZE.erp_cust_az12 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'BRONZE.erp_cust_az12 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
         -- erp_loc_a10
         SET @start_time = GETDATE();
         IF OBJECT_ID('BRONZE.erp_loc_a10', 'U') IS NOT NULL
-            RAISERROR('❌ BRONZE.erp_loc_a10 already exists. Creation blocked.', 16, 1);
+            RAISERROR('BRONZE.erp_loc_a10 already exists. Creation blocked.', 16, 1);
         ELSE
             CREATE TABLE BRONZE.erp_loc_a10 (
                 CID NVARCHAR(50),
                 CNTRY VARCHAR(50)
             );
         SET @end_time = GETDATE();
-        PRINT '✅ BRONZE.erp_loc_a10 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'BRONZE.erp_loc_a10 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
         -- erp_px_cat_g1v2
         SET @start_time = GETDATE();
         IF OBJECT_ID('BRONZE.erp_px_cat_g1v2', 'U') IS NOT NULL
-            RAISERROR('❌ BRONZE.erp_px_cat_g1v2 already exists. Creation blocked.', 16, 1);
+            RAISERROR('BRONZE.erp_px_cat_g1v2 already exists. Creation blocked.', 16, 1);
         ELSE
             CREATE TABLE BRONZE.erp_px_cat_g1v2 (
                 ID NVARCHAR(50),
@@ -100,13 +100,13 @@ BEGIN
                 MAINTENANCE NVARCHAR(50)
             );
         SET @end_time = GETDATE();
-        PRINT '✅ BRONZE.erp_px_cat_g1v2 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'BRONZE.erp_px_cat_g1v2 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
-        PRINT '✅ BRONZE TABLE SETUP COMPLETED SUCCESSFULLY';
+        PRINT 'BRONZE TABLE SETUP COMPLETED SUCCESSFULLY';
 
     END TRY
     BEGIN CATCH
-        PRINT '⚠️ ERROR OCCURRED DURING BRONZE SETUP';
+        PRINT 'ERROR OCCURRED DURING BRONZE SETUP';
         PRINT ERROR_MESSAGE();
     END CATCH
 END;
