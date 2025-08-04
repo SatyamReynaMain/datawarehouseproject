66f3fbbf27d660f5a4f307c1b3b0cca6acc8e3cd
SatyamReynaMain <mygamingsetup10008@gmail.com>
2025-08-04 14:26:52 +0530

    Update ddl_silver_layer.sql

--- Scripts/Silver/ddl_silver_layer.sql
+++ Scripts/Silver/ddl_silver_layer.sql
@@ -1,136 +1,97 @@
---EXEC SILVER.load_silver;
+-- same as bronze discription 
 USE SQLPROJECT1;
 GO
-
-CREATE OR ALTER PROCEDURE SILVER.load_silver AS
+--EXEC SILVER.load_silver1;
+CREATE OR ALTER PROCEDURE SILVER.load_silver1 AS 
 BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME;
 
     BEGIN TRY
-        PRINT 'SILVER LAYER - TABLE CREATION';
-        PRINT '==========================================';
+        PRINT ' SILVER LAYER START';
+        PRINT '============================';
+        PRINT 'CRM SECTION';
+        PRINT '============================';
 
-
-		
-        -- 1 CRM_cust_info
+        -- CRM_cust_info
         SET @start_time = GETDATE();
-        IF OBJECT_ID('Silver.CRM_cust_info', 'U') IS NOT NULL
-            RAISERROR('Silver.CRM_cust_info already exists. Creation blocked.', 16, 1);
-        ELSE
-            CREATE TABLE Silver.CRM_cust_info (
-                cst_id INT,
-                cst_key NVARCHAR(50),
-                cst_firstname NVARCHAR(50),
-                cst_lastname NVARCHAR(50),
-                cst_marital_status NVARCHAR(50),
-                cst_gndr NVARCHAR(50),
-                cst_create_date DATE,
-				dwh_create_date DATETIME2 DEFAULT GETDATE() -- this extra column represents one of metadata(sudo)
-            );
+        PRINT 'TRUNCATE: SILVER.CRM_cust_info';
+        TRUNCATE TABLE SILVER.CRM_cust_info;
+
+        PRINT 'INSERT: SILVER.CRM_cust_info';
+        BULK INSERT SILVER.CRM_cust_info
+        FROM 'C:\SQL project satyam\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        WITH (FIRSTROW=2, FIELDTERMINATOR=',', ROWTERMINATOR='\n', TABLOCK);
         SET @end_time = GETDATE();
-        PRINT 'Silver.CRM_cust_info created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '>> LOAD TIME: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
-
-
-        -- 2 CRM_prd_info
+        -- CRM_prd_info
         SET @start_time = GETDATE();
-        IF OBJECT_ID('Silver.CRM_prd_info', 'U') IS NOT NULL
-            RAISERROR('Silver.CRM_prd_info already exists. Creation blocked.', 16, 1);
-        ELSE
-            CREATE TABLE Silver.CRM_prd_info (
-                prd_id INT,
-                prd_key NVARCHAR(50),
-                prd_nm NVARCHAR(50),
-                prd_cost INT,
-                prd_line NVARCHAR(50),
-                prd_start_dt DATETIME,
-                prd_end_dt DATETIME,
-				dwh_create_date DATETIME2 DEFAULT GETDATE() -- this extra column represents one of metadata(sudo)
-            );
+        PRINT 'TRUNCATE: SILVER.CRM_prd_info';
+        TRUNCATE TABLE SILVER.CRM_prd_info;
+
+        PRINT 'INSERT: SILVER.CRM_prd_info';
+        BULK INSERT SILVER.CRM_prd_info
+        FROM 'C:\SQL project satyam\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+        WITH (FIRSTROW=2, FIELDTERMINATOR=',', ROWTERMINATOR='\n', TABLOCK);
         SET @end_time = GETDATE();
+        PRINT '>> LOAD TIME: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
-        PRINT 'Silver.CRM_prd_info created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
-
-       
-	   
-
-		-- 3 CRM_sales_details
+        -- CRM_sales_details
         SET @start_time = GETDATE();
-        IF OBJECT_ID('Silver.CRM_sales_details', 'U') IS NOT NULL
-            RAISERROR('Silver.CRM_sales_details already exists. Creation blocked.', 16, 1);
-        ELSE
-            CREATE TABLE Silver.CRM_sales_details (
-                sls_ord_num NVARCHAR(50),
-                sls_prd_key NVARCHAR(50),
-                sls_cust_id INT,
-                sls_order_dt INT,
-                sls_ship_dt INT,
-                sls_due_dt INT,
-                sls_sales INT,
-                sls_quantity INT,
-                sls_priceINT INT,
-				dwh_create_date DATETIME2 DEFAULT GETDATE() -- this extra column represents one of metadata(sudo)
-            );
-        SET @end_time = GETDATE();
-        PRINT 'Silver.CRM_sales_details created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'TRUNCATE: SILVER.CRM_sales_details';
+        TRUNCATE TABLE SILVER.CRM_sales_details;
 
-       
-	
-	
-	   --4 erp_cust_az12
+        PRINT 'INSERT: SILVER.CRM_sales_details';
+        BULK INSERT SILVER.CRM_sales_details
+        FROM 'C:\SQL project satyam\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        WITH (FIRSTROW=2, FIELDTERMINATOR=',', ROWTERMINATOR='\n', TABLOCK);
+        SET @end_time = GETDATE();
+        PRINT '>> LOAD TIME: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+
+        PRINT '============================';
+        PRINT 'ERP SECTION';
+        PRINT '============================';
+
+        -- erp_cust_az12
         SET @start_time = GETDATE();
-        IF OBJECT_ID('Silver.erp_cust_az12', 'U') IS NOT NULL
-            RAISERROR('Silver.erp_cust_az12 already exists. Creation blocked.', 16, 1);
-        ELSE
-            CREATE TABLE Silver.erp_cust_az12 (
-                CID NVARCHAR(50),
-                BDATE DATE,
-                GEN VARCHAR(50),
-				dwh_create_date DATETIME2 DEFAULT GETDATE() -- this extra column represents one of metadata(sudo)
-            );
-        SET @end_time = GETDATE();
-        PRINT 'Silver.erp_cust_az12 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'TRUNCATE: SILVER.erp_cust_az12';
+        TRUNCATE TABLE SILVER.erp_cust_az12;
 
-       
-	   
-	 
-	 
-	   --5 erp_loc_a10
+        PRINT 'INSERT: SILVER.erp_cust_az12';
+        BULK INSERT SILVER.erp_cust_az12
+        FROM 'C:\SQL project satyam\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+        WITH (FIRSTROW=2, FIELDTERMINATOR=',', ROWTERMINATOR='\n', TABLOCK);
+        SET @end_time = GETDATE();
+        PRINT '>> LOAD TIME: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+
+        -- erp_loc_a10
         SET @start_time = GETDATE();
-        IF OBJECT_ID('Silver.erp_loc_a10', 'U') IS NOT NULL
-            RAISERROR('Silver.erp_loc_a10 already exists. Creation blocked.', 16, 1);
-        ELSE
-            CREATE TABLE Silver.erp_loc_a10 (
-                CID NVARCHAR(50),
-                CNTRY VARCHAR(50),
-				dwh_create_date DATETIME2 DEFAULT GETDATE() -- this extra column represents one of metadata(sudo)
-            );
-        SET @end_time = GETDATE();
-        PRINT 'Silver.erp_loc_a10 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT 'TRUNCATE: SILVER.erp_loc_a10';
+        TRUNCATE TABLE SILVER.erp_loc_a10;
 
-     
-	 
-	 
-	 --6 erp_px_cat_g1v2
+        PRINT 'INSERT: SILVER.erp_loc_a10';
+        BULK INSERT SILVER.erp_loc_a10
+        FROM 'C:\SQL project satyam\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+        WITH (FIRSTROW=2, FIELDTERMINATOR=',', ROWTERMINATOR='\n', TABLOCK);
+        SET @end_time = GETDATE();
+        PRINT '>> LOAD TIME: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+
+        -- erp_px_cat_g1v2
         SET @start_time = GETDATE();
-        IF OBJECT_ID('Silver.erp_px_cat_g1v2', 'U') IS NOT NULL
-            RAISERROR('Silver.erp_px_cat_g1v2 already exists. Creation blocked.', 16, 1);
-        ELSE
-            CREATE TABLE Silver.erp_px_cat_g1v2 (
-                ID NVARCHAR(50),
-                CAT NVARCHAR(50),
-                SUBCAT NVARCHAR(50),
-                MAINTENANCE NVARCHAR(50),
-				dwh_create_date DATETIME2 DEFAULT GETDATE() -- this extra column represents one of metadata(sudo)
-            );
+        PRINT 'TRUNCATE: SILVER.erp_px_cat_g1v2';
+        TRUNCATE TABLE SILVER.erp_px_cat_g1v2;
+
+        PRINT 'INSERT: SILVER.erp_px_cat_g1v2';
+        BULK INSERT SILVER.erp_px_cat_g1v2
+        FROM 'C:\SQL project satyam\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+        WITH (FIRSTROW=2, FIELDTERMINATOR=',', ROWTERMINATOR='\n', TABLOCK);
         SET @end_time = GETDATE();
-        PRINT 'Silver.erp_px_cat_g1v2 created in ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
+        PRINT '>> LOAD TIME: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' seconds';
 
-        PRINT 'SILVER TABLE SETUP COMPLETED SUCCESSFULLY';
-
+        PRINT 'SILVER LAYER LOAD COMPLETED';
     END TRY
     BEGIN CATCH
-        PRINT 'ERROR OCCURRED DURING SILVER SETUP';
+        PRINT 'ERROR OCCURRED!';
         PRINT ERROR_MESSAGE();
     END CATCH
 END;
